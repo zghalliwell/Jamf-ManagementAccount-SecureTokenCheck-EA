b92cab5ca57a278ab826daf3addf9c3c8e4e5bbb
Zach Halliwell <44303844+zghalliwell@users.noreply.github.com>
2019-07-12 09:32:47 -0500

    Update SecureTokenEA.sh

--- SecureTokenEA.sh
+++ SecureTokenEA.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
 #################################################################
-#	This extension attribute for Jamf Pro will reach out	#
-#	to Jamf via the API, find the username of the		#
-#	management account on the machine and then check on 	#
-#	the Secure Token status of the management account	#
-#	and report it back to Jamf Pro.				#
-#								#
-#	This script encrypts the API username and pass to 	#
-#	protect company info. To generate an			#
-#	encrypted string, salt and password you can use my	#
-#	Mr Encryptor tool here: 				#
-#	https://github.com/zghalliwell/MrEncryptor		#
-#################################################################
-
+# This extension attribute for Jamf Pro will reach out	
+# to Jamf via the API, find the username of the		
+# management account on the machine and then check on 	
+# the Secure Token status of the management account	
+# and report it back to Jamf Pro.
+#
+# Please enter your Jamf Pro address in the variable below:
+#######################
+jamfProURL="my.jamf.pro"
+#######################
+# NOTE: leave out the "https://"
+#								
+#################
+# AUTHENTICATION
+#################
+#
+# This script utilizes 256 bit encryption to protect the username and password 
+# of the API account. Since this will be in a Jamf Pro Extension Attribute, we can't pass parameters down,
+# so we'll need to bake in all three pieces of the encryption key.    
+# Enter your ENCRYPTED STRING, SALT, and PASSPHRASE for both the username and the password
+# in the variables below to decrypt them.
+#
+###########################################
+usernameSTRING="INSERT_ENCRYPTED_STRING_HERE"
+usernameSALT="INSERT_SALT_HERE"
+usernamePASSPHRASE="INSERT_PASSPHRASE_HERE"
+passwordSTRING="INSERT_ENCRYPTED_STRING_HERE"
+passwordSALT="INSERT_SALT_HERE"
+passwordPASSPHRASE="INSERT_PASSPHRASE_HERE"
+###########################################
+#
+# You can use my Mr Encryptor script located at the link below to encrypt
+# your username and password and generate the salt and passphrase:
+#
+# https://github.com/zghalliwell/MrEncryptor
+#     			   
+# The API account will need read-only access to computer inventory records										         
+#      
+################################################################################
 #Establish the function to decrypt the username and password of the API account
 function DecryptString() {
 	echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
 }
-
-#Enter your encrypted string, salt, and passphrase for your API username
-usernameString="ENTER STRING HERE"
-usernameSalt="ENTER SALT HERE"
-usernamePassphrase="ENTER PASSPHRASE HERE"
-
-#Enter your encrypted string, salt, and passphrase for your API password
-passwordString="ENTER STRING HERE"
-passwordSalt="ENTER SALT HERE"
-passwordPassphrase="ENTER PASSPHRASE HERE"
-
-#Enter your Jamf Pro URL here WITHOUT the https://
-jamfProURL="my.jamfpro.url"
 
 #Format for Decryption: DecryptString "parameter for encryption string" "Salt" "Pasphrase"
 apiUser=$(DecryptString "$usernameString" "$usernameSalt" "$usernamePassphrase")
